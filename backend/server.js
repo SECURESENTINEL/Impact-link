eb331c1bb1d7178a9eba6703140c42b5961ece6d
SECURESENTINEL <shriyakhaithwas@gmail.com>
2026-02-24 19:28:03 +0530

    fixed render port

--- backend/server.js
+++ backend/server.js
@@ -18,4 +18,6 @@ app.use("/api/auth", authRoutes);
 app.use("/api/activities", activityRoutes);
 app.use("/api/notifications", notificationRoutes);
 
-app.listen(5000, () => console.log("Server running on 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
