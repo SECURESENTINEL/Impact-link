ad93ac9354529507aa1ba4a3fbe8fdcb65d45730
SECURESENTINEL <shriyakhaithwas@gmail.com>
2026-02-24 20:05:08 +0530

    fixed email transporter

--- backend/routes/activityRoutes.js
+++ backend/routes/activityRoutes.js
@@ -8,11 +8,13 @@ const Notification = require("../models/Notification");
 
 const router = express.Router();
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
-  }
+    pass: process.env.EMAIL_PASS,
+  },
 });
 
 async function sendMail(to, subject, html) {
