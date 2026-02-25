c4d16972cf498ade34dc20e13876cc86b21972a2
SECURESENTINEL <shriyakhaithwas@gmail.com>
2026-02-25 20:59:50 +0530

    final email fix

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -8,9 +8,7 @@ const Volunteer = require("../models/Volunteer");
 const router = express.Router();
 
 const transporter = nodemailer.createTransport({
-  host: "smtp-relay.brevo.com",
-  port: 2525,
-  secure: false,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
